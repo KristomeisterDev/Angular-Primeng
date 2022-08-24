3d42b754c71b37ba557d281edec27826e25abfda
Cristian <cristianluru@gmail.com>
2022-08-24 02:18:06 -0500

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeng';
+   //slider setting variable
+   responsiveOptions;
+   //define validable to store dynamic products data
+   products:any;
+   constructor(private http: HttpClient){
+    //slider responsive settings
+     this.responsiveOptions = [
+       {
+           breakpoint: '1024px',
+           numVisible: 3,
+           numScroll: 3
+       },
+       {
+           breakpoint: '768px',
+           numVisible: 2,
+           numScroll: 2
+       },
+       {
+           breakpoint: '560px',
+           numVisible: 1,
+           numScroll: 1
+       }
+   ];
+    //get request
+     this.http.get('https://www.testjsonapi.com/products/').subscribe(data => {
+       //data storing for use in html component
+       this.products = data;
+           }, error => console.error(error));
+ } 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { CarouselModule } from 'primeng/carousel';
+import { ButtonModule } from 'primeng/button';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CarouselModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
